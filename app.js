7243eb9790d20a137b434f11800a56bf4f919e1d
NYX <nyxv3il@proton.me>
2025-11-18 15:14:16 +0530

    Updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const HOST = "localhost";
+const HOST = "0.0.0.0";
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
