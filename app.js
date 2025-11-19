de57dbad9697ed5c3a2bd851baf015c9be13be8a
NYX <nyxv3il@proton.me>
2025-11-19 14:09:33 +0530

    test

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  // res.sendFile(path.join(__dirname, "frontend", "index.html"));
-  res.send("Hello, World!");
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 app.get("/health", (req, res) => {
